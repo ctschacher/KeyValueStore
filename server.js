90bd53ba02beabebf342db4551350b79ad935a2b
Christian Tschacher <ctschacher@axway.com>
2019-09-07 12:42:55 -0400

    Added request responses

--- server.js
+++ server.js
@@ -10,8 +10,24 @@ mongoose.connect('mongodb://root:example@127.0.0.1:27017/admin', { useNewUrlPars
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// Listener
+app.post("/v1/key/:key/value/:value",(req,res,next)=>{
+    console.log('Received POST request with key/value: ', req.params.key, '/', req.params.value);
+    res.send('Key/value accepted');
+});
+
+app.get('/v1/key/:key',(req,res,next)=>{
+    console.log('Received GET request for key: ', req.params.key);
+    res.send('Getting Key/Value ');
+})
+
+app.delete('/v1/key/:key', (req,res,next)=>{
+    console.log('Received DELETE request for key: ', req.params.key);
+    res.send('Deleting key');
+})
+
+
 var port = 8080;
+
 app.listen(port, () => {
     console.log('Server is up and running on port number ' + port);
 });

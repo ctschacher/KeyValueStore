6e8361eac318f3ea26d7de3f487efa5580231fc7
Christian Tschacher <ctschacher@axway.com>
2019-09-08 22:23:56 +0200

    Improved behaviour responses of API requests

--- routes/keyValue.route.js
+++ routes/keyValue.route.js
@@ -7,36 +7,35 @@ var KeyValue = require('../models/keyValue.model');
 // Define request responses
 router.post("/key/:key/value/:value", (req, res, next) => {
     console.log('Received POST request with key/value: ', req.params.key, '/', req.params.value);
-    var keyValue = new KeyValue(
-        {
-            key: req.params.key,
-            value: req.params.value
-        }
-    );
 
-    keyValue.save(function (err) {
-        if (err) {
-            return next(err);
+    // If key already exist then update otherwise create it
+    KeyValue.findOneAndUpdate({ key: req.params.key }, { value: req.params.value }, { upsert: true }, (error) => {
+        if (error) {
+            return next(err)
         }
         res.send('Key/Value created successfully')
-    })
-});
+    }
+    );
+})
 
 router.get('/key/:key', (req, res, next) => {
     console.log('Received GET request for key: ', req.params.key);
     KeyValue.findOne({ key: req.params.key }, function (err, obj) {
-        if (err) {
-            res.status(404).send('Not found');
-            return next(err);   
-        } else
-            res.send(obj.value);
+        if (err || obj == null) {
+            res.status(404).send('Key not found');
+            return next(err);
+        }
+        res.send(obj.value);
     })
 })
 
 router.delete('/key/:key', (req, res, next) => {
     console.log('Received DELETE request for key: ', req.params.key);
-    KeyValue.findOneAndRemove(req.params.id, function (err) {
-        if (err) return next(err);
+    KeyValue.findOneAndRemove({ key: req.params.key }, function (err, obj) {
+        if (err || obj == null) {
+            res.status(404).send('Key not found');
+            return next(err);
+        }
         res.send('Key deleted successfully');
     })
 })
